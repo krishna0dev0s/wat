479d358b1965fc38955fa12b98fd9a906e0271cf
krishna0dev0s <kg933617@gmail.com>
2025-11-23 00:03:47 +0530

    Fix Prisma deployment issues: Add binaryTargets, update build script, and webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,12 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("_http_common");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
