77ea14eaac4b6338ba85125072d74370151faeda
krishna0dev0s <kg933617@gmail.com>
2025-11-23 12:29:34 +0530

    Remove BackgroundWrapper import from main layout

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,13 +1,10 @@
 import React from "react";
-import BackgroundWrapper from "@/components/background-wrapper";
 
 const MainLayout = async ({ children }) => {
   return (
-    <BackgroundWrapper>
-      <div className="min-h-screen pt-16 pb-20">
-        {children}
-      </div>
-    </BackgroundWrapper>
+    <div className="min-h-screen pt-16 pb-20">
+      {children}
+    </div>
   );
 };
 
